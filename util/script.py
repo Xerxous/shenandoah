bfa04a0264043ee9d1b3e7e1b9c8e555f8106e33
jackson <woosuk2009@gmail.com>
2017-02-17 10:54:15 -0500

    reading xls complete

--- util/script.py
+++ util/script.py
@@ -1,22 +1,34 @@
-import os
 from xlrd import open_workbook
 
-
+SHEET_INDEX = 0
 FIRST_ROW = 0
 SECOND_ROW = 1
-PATH = 'home/jackson/examples.xlsx'
+ENTRY_START_ROW = 2
+ADDRESS_START = 5
+ADDRESS_END = 10
+PATH = '/home/jackson/examples.xlsx'
 
 book = open_workbook(PATH)
-sheet = book.sheet_by_index(0)
+sheet = book.sheet_by_index(SHEET_INDEX)
 
 #LABEL ROWS(1 and 2)
 row_one = [sheet.cell(FIRST_ROW, col).value for col in range(sheet.ncols)]
 row_two = [sheet.cell(SECOND_ROW, col).value for col in range(sheet.ncols)]
 
 #FORMAT and MERGE into one to eliminate empty cells
-columns = row_one[:5] + row_two[5:9] + row_one[10:]
-print(columns)
+columns = row_one[:ADDRESS_START] + row_two[ADDRESS_START:ADDRESS_END] + row_one[ADDRESS_END:]
 
-dict_list = []
-# for row_index in range(1, sheet.nrows):
-#     sheet.cell(row_index, col_index).value
+#row entries
+entries = []
+for col_index in range(len(columns)):
+    col_list = []
+    for row_index in range(ENTRY_START_ROW, sheet.nrows):
+        col_list.append(sheet.cell(row_index, col_index).value)
+    entries.append(col_list)
+
+#dictionary - key value pair of each columns label and a list of their entries
+dict_list = dict()
+for index in range(len(columns)):
+    dict_list[columns[index]] = entries[index]
+
+#load in db
